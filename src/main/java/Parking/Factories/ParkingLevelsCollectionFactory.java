bcf2c0321e7e0644c73529b69bf1774ac164eea9
Kunda Shyam <shyam.k@shyam.k>
2022-07-04 18:45:28 +0530

    Added multiple spots for each type

--- src/main/java/Parking/Factories/ParkingLevelsCollectionFactory.java
+++ src/main/java/Parking/Factories/ParkingLevelsCollectionFactory.java
@@ -10,10 +10,18 @@ public class ParkingLevelsCollectionFactory {
     private int NUM_OF_SPOT_TYPES;
     private int NUM_OF_SPOTS_PER_SPOT_TYPE ;
 
+    private Map<ParkingSpotType, Integer> NUM_OF_SPOTS_PER_SPOT_TYPE_Map ;
+
     public ParkingLevelsCollectionFactory(int NUM_OF_LEVELS, int NUM_OF_SPOT_TYPES, int NUM_OF_SPOTS_PER_SPOT_TYPE) {
         this.NUM_OF_LEVELS = NUM_OF_LEVELS;
         this.NUM_OF_SPOT_TYPES = NUM_OF_SPOT_TYPES;
         this.NUM_OF_SPOTS_PER_SPOT_TYPE = NUM_OF_SPOTS_PER_SPOT_TYPE;
+    }
+
+    public ParkingLevelsCollectionFactory(int NUM_OF_LEVELS, int NUM_OF_SPOT_TYPES, Map<ParkingSpotType, Integer> NUM_OF_SPOTS_PER_SPOT_TYPE_Map) {
+        this.NUM_OF_LEVELS = NUM_OF_LEVELS;
+        this.NUM_OF_SPOT_TYPES = NUM_OF_SPOT_TYPES;
+        this.NUM_OF_SPOTS_PER_SPOT_TYPE_Map = NUM_OF_SPOTS_PER_SPOT_TYPE_Map;
     }
 
     public int getNUM_OF_LEVELS() {
@@ -36,17 +44,36 @@ public class ParkingLevelsCollectionFactory {
         return NUM_OF_SPOTS_PER_SPOT_TYPE;
     }
 
+    public int getNUM_OF_SPOTS_PER_SPOT_TYPE(Map<ParkingSpotType, Integer> spotAllocations, ParkingSpotType parkingSpotType) {
+        return spotAllocations.get(parkingSpotType);
+    }
+
     public void setNUM_OF_SPOTS_PER_SPOT_TYPE(int NUM_OF_SPOTS_PER_SPOT_TYPE) {
         this.NUM_OF_SPOTS_PER_SPOT_TYPE = NUM_OF_SPOTS_PER_SPOT_TYPE;
     }
+
 
     public  ParkingLevelsCollection createParkingLevelsCollection() {
         ParkingLevelsCollection levels =
                 new ArrayListParkingLevelsCollection();
         int parkingSpotID = 0;
         int parkingLevelID = 0;
+        Map<ParkingSpotType, Collection<ParkingSpot>> allSpotsForLevel;
         for (int i = 0; i < getNUM_OF_LEVELS(); i++) {
-            Map<ParkingSpotType, Collection<ParkingSpot>> allSpotsForLevel = generateSpots(parkingLevelID, parkingSpotID);
+            if (i < getNUM_OF_LEVELS()-1) {
+                allSpotsForLevel = generateSpots(parkingLevelID, parkingSpotID, NUM_OF_SPOTS_PER_SPOT_TYPE_Map,getNUM_OF_LEVELS() );
+            } else {
+                Map<ParkingSpotType, Integer> remainingSpots = new HashMap<>();
+                for (ParkingSpotType parkingSpotType : ParkingSpotType.values()) {
+                    int totalVacantSpots = 0;
+                    for(ParkingLevel level : levels ){
+                        totalVacantSpots += level.getNumOfVacantSpotsOfType(parkingSpotType);
+                    }
+                    remainingSpots.put(parkingSpotType, NUM_OF_SPOTS_PER_SPOT_TYPE_Map.get(parkingSpotType) - totalVacantSpots);
+                }
+                allSpotsForLevel = generateSpotsLastLevel(parkingLevelID, parkingSpotID, remainingSpots,getNUM_OF_LEVELS() );
+            }
+
 
             VehicleToParkingSpotTypeMapper vehicleToSpotTypesMapper = typesMapper();
             ParkingLevel parkingLevel = new CParkingLevel(parkingLevelID, allSpotsForLevel,
@@ -55,6 +82,7 @@ public class ParkingLevelsCollectionFactory {
             parkingSpotID += NUM_OF_SPOT_TYPES * NUM_OF_SPOTS_PER_SPOT_TYPE;
             parkingLevelID++;
         }
+
         return levels;
     }
 
@@ -62,11 +90,9 @@ public class ParkingLevelsCollectionFactory {
         Map<VehicleType, Collection<ParkingSpotType>> typesMapper =
                 new HashMap<>();
         typesMapper.put(VehicleType.MOTORCYCLE,
-                Arrays.asList(ParkingSpotType.MOTORCYCLE,
-                        ParkingSpotType.COMPACT,
-                        ParkingSpotType.LARGE));
+                Arrays.asList(ParkingSpotType.MOTORCYCLE));
         typesMapper.put(VehicleType.CAR,
-                Arrays.asList(ParkingSpotType.COMPACT, ParkingSpotType.LARGE));
+                Arrays.asList(ParkingSpotType.COMPACT));
         typesMapper.put(VehicleType.TRUCK,
                 Collections.singletonList(ParkingSpotType.LARGE));
         return typesMapper::get;
@@ -85,4 +111,37 @@ public class ParkingLevelsCollectionFactory {
         }
         return allSpotsForLevel;
     }
+
+    private  Map<ParkingSpotType, Collection<ParkingSpot>> generateSpots(int parkingLevelID, int parkingSpotID, Map<ParkingSpotType, Integer> spotAllocations, int totalLevels) {
+        Map<ParkingSpotType, Collection<ParkingSpot>> allSpotsForLevel = new HashMap<>();
+        for (ParkingSpotType parkingSpotType : ParkingSpotType.values()) {
+            Collection<ParkingSpot> spotsForLevel = new ArrayList<>();
+            int totalSpotForEachTypeInEachLevel;
+            totalSpotForEachTypeInEachLevel = spotAllocations.get(parkingSpotType) / totalLevels;
+            for (int i = 0; i < totalSpotForEachTypeInEachLevel; i++) {
+                spotsForLevel.add(new ParkingSpot(parkingSpotID,
+                        parkingLevelID, parkingSpotType));
+                parkingSpotID++;
+            }
+            allSpotsForLevel.put(parkingSpotType, spotsForLevel);
+        }
+        return allSpotsForLevel;
+    }
+
+    private  Map<ParkingSpotType, Collection<ParkingSpot>> generateSpotsLastLevel(int parkingLevelID, int parkingSpotID, Map<ParkingSpotType, Integer> remainingSpots, int totalLevels) {
+        Map<ParkingSpotType, Collection<ParkingSpot>> allSpotsForLevel = new HashMap<>();
+        for (ParkingSpotType parkingSpotType : ParkingSpotType.values()) {
+            Collection<ParkingSpot> spotsForLevel = new ArrayList<>();
+            int totalSpotForEachTypeInEachLevel;
+            totalSpotForEachTypeInEachLevel = remainingSpots.get(parkingSpotType);
+            for (int i = 0; i < totalSpotForEachTypeInEachLevel; i++) {
+                spotsForLevel.add(new ParkingSpot(parkingSpotID,
+                        parkingLevelID, parkingSpotType));
+                parkingSpotID++;
+            }
+            allSpotsForLevel.put(parkingSpotType, spotsForLevel);
+        }
+        return allSpotsForLevel;
+    }
+
 }
--- src/main/java/Parking/Factories/ParkingLotFactory.java
+++ src/main/java/Parking/Factories/ParkingLotFactory.java
@@ -2,6 +2,7 @@ package Parking.Factories;
 
 import Parking.ParkingLevelsCollection;
 import Parking.ParkingLot;
+import Parking.ParkingSpotType;
 import Parking.Policies.BasicLevelAssignmentPolicy;
 import Parking.Policies.BasicParkingAssignmentPolicy;
 import Parking.Policies.LevelAssignmentPolicy;
@@ -9,9 +10,11 @@ import Parking.Policies.ParkingAssignmentPolicy;
 import Payment.FeeModel;
 
 import java.util.List;
+import java.util.Map;
 
 public class ParkingLotFactory {
-    public ParkingLot createNewParkingLot(int numOfLevels, int parkingSpotTypes, int noOfSpotsPerSpotType, List<FeeModel> feeModelList) {
+
+    public ParkingLot createNewParkingLot(int numOfLevels, int parkingSpotTypes, Map<ParkingSpotType, Integer> noOfSpotsPerSpotType, List<FeeModel> feeModelList) {
         ParkingLevelsCollectionFactory parkingLevelsCollectionFactory = new ParkingLevelsCollectionFactory(numOfLevels, parkingSpotTypes, noOfSpotsPerSpotType);
         ParkingLevelsCollection parkingLevels = parkingLevelsCollectionFactory.createParkingLevelsCollection();
         LevelAssignmentPolicy levelAssignmentPolicy = new BasicLevelAssignmentPolicy();
